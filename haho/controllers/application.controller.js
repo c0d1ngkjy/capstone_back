adb95ae8e751c99b9044334a2525a680c1b0ba00
haho0304 <89858900+haho0304@users.noreply.github.com>
2024-06-07 17:36:27 +0900

    a
    a

--- haho/controllers/application.controller.js
+++ haho/controllers/application.controller.js
@@ -2,9 +2,10 @@ const Applications = require("../services/application.service.js");
 
 module.exports.addApplication = async(req, res, next) => {
     const { title, dateRange, questionList, clubId } = req.body;
+    const { from, to } = dateRange;
 
     const applications = new Applications();
-    const application = await applications.addApplication(title, questionList, ...dateRange, clubId);
+    const application = await applications.addApplication(title, questionList, from, to, clubId);
 
     if(application) return res.status(200).json({msg : "신청서 생성", applicationData : application})
     else return res.status(500).json({ msg: "신청서 생성 오류" });
