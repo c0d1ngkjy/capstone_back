61dcc28a457502a9c1ba90677d085af0c7a5cf87
haho0304 <89858900+haho0304@users.noreply.github.com>
2024-06-12 21:21:52 +0900

    Update user.js

--- haho/models/user.js
+++ haho/models/user.js
@@ -34,7 +34,7 @@ module.exports = function(sequelize, DataTypes) {
             }
         }, 
         image: {
-            type: DataTypes.STRING(1000),
+            type: DataTypes.STRING(1500),
             allowNull: true,
         }, 
         studentId: {
