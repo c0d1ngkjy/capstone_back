3b7a2430fd068408ca65da75db8b4b6e654deada
haho0304 <89858900+haho0304@users.noreply.github.com>
2024-06-12 19:44:57 +0900

    Update application.route.js

--- haho/routes/application.route.js
+++ haho/routes/application.route.js
@@ -7,6 +7,6 @@ router.post("/addApplication", application.addApplication); //create하기
 router.post("/get", application.getApplication); 
 router.post("/delete", application.deleteApplication);
 router.post("/getlink", application.getApplicationFromLink);
-router.post("addAnswer", application.addAnswer);
+router.post("/addAnswer", application.addAnswer);
 
 module.exports = router;
